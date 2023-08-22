14da9f96399272526e5a9d2dc3f88e021226391a
Mkznd <yzundai1@gmail.com>
2023-08-22 21:32:12 +0200

    First day of advent of code 2022, wrote a bruteforce TS solution to a simple program

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+function sum_n_last_values(arr, n) {
+    let sum = 0;
+    for (let i = 0; i < n; i++) {
+        sum += arr[arr.length - 1 - i];
+    }
+    return sum;
+}
+function bubble_sort(inp) {
+    let nums = inp;
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = 0; j < nums.length - i - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                let tmp = nums[j];
+                nums[j] = nums[j + 1];
+                nums[j + 1] = tmp;
+            }
+        }
+    }
+    return nums;
+}
+fs_1.default.readFile('./adventofcode.com_2022_day_1_input.txt', (err, inputId) => {
+    let nums = [];
+    let num = 0;
+    let tmp = "";
+    for (let i of inputId.toString()) {
+        if (i == '\n') {
+            if (tmp != "") {
+                num += (Number(tmp));
+                tmp = "";
+            }
+            else {
+                nums.push(num);
+                num = 0;
+            }
+        }
+        else {
+            tmp += i;
+        }
+    }
+    let max = nums[0];
+    let sum = 0;
+    console.log(sum_n_last_values(bubble_sort(nums), 3));
+});
